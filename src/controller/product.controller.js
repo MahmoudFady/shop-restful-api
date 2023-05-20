16fa6e550358c3121e2fa108478fe1550b5f7b97
keta <ahmedketa12@gmail.com>
2023-05-20 21:47:42 +0300

    Re edit product routes

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -1,9 +1,177 @@
-const productModel = require("../models/product.model");
-module.exports.createOne = async (req, res, next) => {
+const path = require("path");
+const fs = require("fs");
+const { getResErr } = require(path.join(
+  __dirname,
+  "..",
+  "utils",
+  "error.util"
+));
+const ProductModel = require(path.join(
+  __dirname,
+  "..",
+  "models",
+  "product.model"
+));
+const { uploadImage } = require(path.join(
+  __dirname,
+  "..",
+  "utils",
+  "upload-img.util"
+));
+const productsPerPage = 12;
+
+exports.createOne = async (req, res, next) => {
+  const newProduct = req.body;
+  const image = req.files.image[0];
   try {
-    const product = await new productModel(req.body).save();
-    res.status(200).json({ message: "product created", id: product.id });
-  } catch (err) {
-    next(err);
+    if (!image) {
+      getResErr("Not valid image.", 422);
+    } else {
+      newProduct.image = await uploadImage(image.path);
+      fs.unlink(path.join(image.path));
+    }
+    const thumbnails = files.thumbnails;
+    if (!thumbnails) {
+      getResErr("Not valid thumbnails.", 422);
+    } else {
+      newProduct.thumbnails = [];
+      for (thumbnail of thumbnails) {
+        newProduct.thumbnails.push(await uploadImage(thumbnail.path));
+        fs.unlink(path.join(thumbnail.path));
+      }
+      const product = await productService.createOne(req.files, req.body);
+      res.status(201).json({ message: "product created", product });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateOne = async (req, res, next) => {
+  try {
+    const result = await new ProductModel(product).save();
+    if (result.modifiedCount === 1) {
+      res.status(201).json({ message: "product updated" });
+    } else {
+      throw new Error("Didn't modify!!");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductDetails = async (req, res, next) => {
+  try {
+    const product = await ProductModel.findOne({ _id }).populate("reviews");
+    res.status(200).json({ message: "ok", product });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInCategoryCount = async (req, res, next) => {
+  try {
+    const count = await ProductModel.countDocuments({
+      category: req.params.category,
+    });
+    res.status(200).json({ message: "ok", count });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInCategory = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({ category: req.params.category })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInCategoryPriceAsc = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({ category: req.params.category })
+      .sort({ price: 1 })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInCategoryPriceDesc = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({ category: req.params.category })
+      .sort({ price: -1 })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInSubCategoryCount = async (req, res, next) => {
+  try {
+    const count = await ProductModel.countDocuments({
+      subCategory: req.params.subcategory,
+    });
+    res.status(200).json({ message: "ok", count });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInSubCategory = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({
+      subCategory: req.params.subCategory,
+    })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInSubCategoryPriceAsc = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({
+      subCategory: req.params.subCategory,
+    })
+      .sort({ price: 1 })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProductsInSubCategoryPriceDesc = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({
+      subCategory: req.params.subCategory,
+    })
+      .sort({ price: -1 })
+      .skip((req.params.page - 1) * productsPerPage)
+      .limit(productsPerPage);
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getAllUnactiveProducts = async (req, res, next) => {
+  try {
+    const products = await ProductModel.find({ active: false });
+    res.status(200).json({ message: "ok", products });
+  } catch (error) {
+    next(error);
   }
 };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,18 @@
+const path = require("path");
 const express = require("express");
-const router = express.Router();
-const productCtrl = require("../controller/product.controller");
-const multerMw = require("../middlewares/img-upload.mw");
-const productFilesMw = multerMw.fields([
+const multerMW = require(path.join(
+  __dirname,
+  "..",
+  "middlewares",
+  "img-upload.mw"
+));
+const productController = require(path.join(
+  __dirname,
+  "..",
+  "controller",
+  "product.controller"
+));
+const productFilesMw = multerMW.fields([
   {
     name: "image",
     maxCount: 1,
@@ -12,6 +22,47 @@ const productFilesMw = multerMw.fields([
     maxCount: 5,
   },
 ]);
-router.route("/product").post(productFilesMw, productCtrl.createOne);
+const router = express.Router();
+
+router
+  .route("/product")
+  .post(productFilesMw, productController.createOne)
+  .put(productController.updateOne);
+
+router.route("/product/:id").get(productController.getProductDetails);
+
+router
+  .route("/product/category/:category")
+  .get(productController.getProductsInCategoryCount);
+
+router
+  .route("/product/category/:category/:page")
+  .get(productController.getProductsInCategory);
+
+router
+  .route("/product/priceasc/category/:category/:page")
+  .get(productController.getProductsInCategoryPriceAsc);
+
+router
+  .route("/product/pricedesc/category/:category/:page")
+  .get(productController.getProductsInCategoryPriceDesc);
+
+router
+  .route("/product/subcategory/:subcategory")
+  .get(productController.getProductsInSubCategoryCount);
+
+router
+  .route("/product/subcategory/:subcategory/:page")
+  .get(productController.getProductsInSubCategory);
+
+router
+  .route("/product/priceasc/subcategory/:subcategory/:page")
+  .get(productController.getProductsInSubCategoryPriceAsc);
+
+router
+  .route("/product/pricedesc/subcategory/:subcategory/:page")
+  .get(productController.getProductsInSubCategoryPriceDesc);
+
+router.route("/product/unactive").get(productController.getAllUnactiveProducts); //for admin to reactivate it
 
 module.exports = router;
