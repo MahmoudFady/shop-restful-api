5b15b10295d3582af679163562eb282616459374
mahmoud fady <mahmoudfady02@gmail.com>
2023-05-26 15:46:41 +0300

    Mahmoud : Fix auth mw

--- src/middlewares/check-auth.mw.js
+++ src/middlewares/check-auth.mw.js
@@ -1,22 +1,16 @@
 const tokenUtil = require("../utils/token.util");
 const errUtil = require("../utils/error.util");
-const userModel = require("../models/user.model");
-try {
-  const token = req.headers["authorization"].split(" ")[1];
-  const user = tokenUtil.verify(token);
-  req["user"] = user;
-  next();
-} catch (err) {
-  next(errUtil(err.message, 204, false));
-}
-
-//admin acceess
-export const isAdmin = async (req, res, next) => {
+module.exports = (req, res, next) => {
   try {
-    const user = await userModel.findById(req.user.id);
-    if (user.role !== 1) throw errUtil("not allowed action", 202, false);
+    const token = req.headers["authorization"].split(" ")[1];
+    const user = tokenUtil.verify(token);
+    req["user"] = user;
     next();
-  } catch (error) {
-    next(err);
+  } catch (err) {
+    next(errUtil(err.message, 204));
   }
+};
+module.exports.isAdmin = async (req, res, next) => {
+  if (req.user.role !== "admin") throw errUtil("action not allowed", 403);
+  next();
 };
