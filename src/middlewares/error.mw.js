6e419cf86f51b9f4962097e18807303f8df21b3f
mahmoud fady <mahmoudfady02@gmail.com>
2023-06-01 00:58:12 +0300

    Mahmoud Fady : Fix error mw logic

--- src/middlewares/error.mw.js
+++ src/middlewares/error.mw.js
@@ -1,4 +1,4 @@
 module.exports = (err, req, res, next) => {
-  const message = err || "Internal Server Error";
+  const message = err.messages || "Internal Server Error";
   res.status(err.status || 500).json({ message });
 };
--- src/utils/error.util.js
+++ src/utils/error.util.js
@@ -1,6 +1,6 @@
 module.exports = (msg, status) => {
   msg = msg || "internal server error";
   const err = new Error(msg);
-  err.status = status || 500;
+  err.status = status || 5000;
   return err;
 };
